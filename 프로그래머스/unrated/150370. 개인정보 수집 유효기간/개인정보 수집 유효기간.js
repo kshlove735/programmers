80ac383e288ecb5a3e3a352745d6d673c7ee9199
Woody <86551233+kshlove735@users.noreply.github.com>
2023-01-19 10:08:00 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.55 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,18 +1,18 @@
 function solution(today, terms, privacies) {
   let answer = [];
-  let expireDate = new Date(today);
+  let basicDate = new Date(today);
   let termsObj = {};
   for (let term of terms) {
-    let termSplit = term.split(" ");
-    termsObj[termSplit[0]] = termSplit[1];
+    let [type, month] = term.split(" ");
+    termsObj[type] = month;
   }
+
   for (let i = 0; i < privacies.length; i++) {
-    let privaciySpit = privacies[i].split(" ");
-    let acceptDate = new Date(privaciySpit[0]);
+    let [startDate, type] = privacies[i].split(" ");
+    let acceptDate = new Date(startDate);
 
-    acceptDate.setMonth(acceptDate.getMonth() + parseInt(termsObj[privaciySpit[1]]));
-
-    if (expireDate.getTime() >= acceptDate.getTime()) {
+    acceptDate.setMonth(acceptDate.getMonth() + parseInt(termsObj[type]));
+    if (basicDate.getTime() >= acceptDate.getTime()) {
       answer.push(i + 1);
     }
   }
