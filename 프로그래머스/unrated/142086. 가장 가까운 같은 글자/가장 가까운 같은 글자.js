2c91043affac40991a98d60fbd56553cf0e38f7a
Woody <86551233+kshlove735@users.noreply.github.com>
2023-01-24 18:25:44 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 4.97 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,13 +1,16 @@
 function solution(s) {
   let answer = [];
   let obj = {};
+
+  // 처음 : -1
+  // 앞에 있있다면 : i - 최근 index
   for (let i = 0; i < s.length; i++) {
-     
-   if(s.substr(0,i).lastIndexOf(s[i])!==-1){
-       answer.push(i-s.substr(0,i).lastIndexOf(s[i]))
-   }else{
-       answer.push(s.substr(0,i).lastIndexOf(s[i]))
-   }
+    if (obj[s[i]] !== undefined) {
+      answer.push(i - obj[s[i]]);
+    } else {
+      answer.push(-1);
+    }
+    obj[s[i]] = i;
   }
   return answer;
 }
