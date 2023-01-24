2a0afab39b51f36f30a59bd5d9973085e119de5a
Woody <86551233+kshlove735@users.noreply.github.com>
2023-01-24 20:04:44 +0900

    [unrated] Title: 문자열 나누기, Time: 7.56 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
@@ -3,19 +3,22 @@ function solution(s) {
   let answer = 0;
   let count = 0;
   let temp = 0;
+  let words = [];
 
   for (let i = 0; i < s.length; i++) {
     s[temp] === s[i] ? count++ : count--;
 
     if (count === 0) {
-      // console.log(s.slice(temp, i + 1));
+      words.push(s.slice(temp, i + 1));
       temp = i + 1;
       answer++;
     }
   }
 
-  // console.log(s.slice(temp, s.length));
-  if (temp < s.length) answer++;
-
+  if (temp < s.length) {
+    words.push(s.slice(temp, s.length));
+    answer++;
+  }
+  console.log(words);
   return answer;
 }
