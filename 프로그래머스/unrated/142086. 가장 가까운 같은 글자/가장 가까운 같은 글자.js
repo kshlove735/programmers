9d93560063c399497fcf0e8840e37148defc163a
Woody <86551233+kshlove735@users.noreply.github.com>
2023-01-24 18:48:04 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.84 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,11 +1,22 @@
-function solution(s) {
-  let answer = [];
-  for (let i = 0; i < s.length; i++) {
-    // if (s.substr(0, i).lastIndexOf(s[i]) !== -1) answer.push(i - s.substr(0, i).lastIndexOf(s[i]));
-    // else answer.push(s.substr(0, i).lastIndexOf(s[i]));
+// 처음 : -1
+// 앞에 있있다면 : i - 최근 index
 
-    let a = s.substr(0, i).lastIndexOf(s[i]) !==-1 ? i-s.substr(0, i).lastIndexOf(s[i]) : s.substr(0, i).lastIndexOf(s[i])
-    answer.push(a)
-  }
-  return answer;
+// 방법1
+function solution(s) {
+  return [...s].map((w, i) => {
+    return s.substr(0, i).lastIndexOf(w) !== -1 ? i - s.substr(0, i).lastIndexOf(w) : -1;
+  });
 }
+// 방법2
+// function solution(s) {
+//   let answer = [];
+//   let obj = {};
+
+//   for (let i = 0; i < s.length; i++) {
+//     if (obj[s[i]] !== undefined) answer.push(i - obj[s[i]]);
+//     else answer.push(-1);
+
+//     obj[s[i]] = i;
+//   }
+//   return answer;
+// }
