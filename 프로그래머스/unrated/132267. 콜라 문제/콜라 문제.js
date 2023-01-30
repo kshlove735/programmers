83b07e16e6aea1fb6942b2fb8612240ccc601d42
Woody <86551233+kshlove735@users.noreply.github.com>
2023-01-30 21:37:45 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,11 +1,8 @@
 function solution(a, b, n) {
   let answer = 0;
-  function helper(a, b, n) {
-    if (n < a) return
+  while (n >= a) {
     answer += Math.floor(n / a) * b;
     n = n - Math.floor(n / a) * a + Math.floor(n / a) * b;
-    helper(a, b, n)
   }
-  helper(a, b, n)
   return answer;
 }
