73c1b00dc7c08451260198c4b4726540e859a1b9
Woody <86551233+kshlove735@users.noreply.github.com>
2023-02-04 18:29:03 +0900

    [level 1] Title: 최소직사각형, Time: 3.26 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,4 +1,17 @@
 function solution(sizes) {
-    var answer = 0;
-    return answer;
+  let answer = 0;
+  let leftMax = 0;
+  let rightMax = 0;
+  sizes.forEach(ele => {
+    if (ele[0] >= ele[1]) {
+      if (ele[0] > leftMax) leftMax = ele[0]
+      if (ele[1] > rightMax) rightMax = ele[1]
+    } else {
+      if (ele[1] > leftMax) leftMax = ele[1]
+      if (ele[0] > rightMax) rightMax = ele[0]
+    }
+  });
+  answer = leftMax * rightMax
+  console.log(leftMax, rightMax);
+  return answer;
 }
