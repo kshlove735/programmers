4545bf0a9a03a18f50b4b51925f9af5fafd4065e
Woody <86551233+kshlove735@users.noreply.github.com>
2023-02-09 20:35:49 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 26.18 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,11 +1,13 @@
 function solution(arr) {
-  let answer = [];
-  answer.push(arr[0])
-  arr.reduce((acc, cur) => {
-    if (acc !== cur) {
-      answer.push(cur)
-    }
-    return cur
-  }, arr[0])
-  return answer;
+  // 방법1
+  return arr.filter((ele, i) => ele != arr[i + 1])
+
+  // 방법 2
+  // let answer = [];
+  // answer.push(arr[0])
+  // arr.reduce((acc, cur) => {
+  //   if (acc !== cur) answer.push(cur)
+  //   return cur
+  // }, arr[0])
+  // return answer;
 }
