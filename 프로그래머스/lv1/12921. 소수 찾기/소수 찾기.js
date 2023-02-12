1983349f7296ab1e043d4b4fc0bfd862435a5fc9
Woody <86551233+kshlove735@users.noreply.github.com>
2023-02-12 19:26:33 +0900

    [level 1] Title: 소수 찾기, Time: 54.71 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,20 +1,15 @@
-
-
 function solution(n) {
   let arr = []
   for (let i = 0; i <= n; i++) {
     arr.push(i)
   }
-  // console.log(arr);
+  
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === 0 || arr[i] === 1) continue;
-    // console.log(arr.length, arr[i]);
-    // arr = arr.filter(num => (num === arr[i]) || (num % arr[i] !== 0));
 
     for (let j = 2; j * i < arr.length; j++) {
       arr[j * i] = 0;
     }
   }
   return arr.filter(num => num !== 0 && num !== 1).length;
-  return arr.length;
 }
