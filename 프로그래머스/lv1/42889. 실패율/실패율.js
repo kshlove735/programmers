f0fd821094fbcf5619aef631dea5ee604518d6ae
Woody <86551233+kshlove735@users.noreply.github.com>
2023-02-13 18:55:18 +0900

    [level 1] Title: 실패율, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,27 +1,9 @@
 function solution(N, stages) {
-  // 스테이지 개수 : 1 ~ N + 1
-  // stages : 현재 유저가 도전 중인 스테이지 번호
-  // N + 1 : 마지막 까지 클리어한 사용자
-  // 실패율 : 현재 도달한 유저의 수 / 스테이지에 도달 한 플레이어 수
-  // 스테이지에 도달한 유저가 없는 경우 해당 스테이지의 실패율 = 0
-
-  let stagesObj = {}
-  stages.forEach(element => {
-    stagesObj[element] = (stagesObj[element] | 0) + 1
-  });
-
   let arr = []
-  for (let i = 0; i < N; i++) {
-    let curr = 0;
-    let stagesFailObj = { name: '', fail: 0 }
-
-    let total = stages.filter(n => n >= i + 1).length
-    if (stagesObj[i + 1]) {
-      curr += stagesObj[i + 1]
-    }
-    stagesFailObj.name = i + 1;
-    stagesFailObj.fail = curr / total
-    arr.push(stagesFailObj)
+  for (let i = 1; i <= N; i++) {
+    let curr = stages.filter(n => n === i).length
+    let total = stages.filter(n => n >= i).length
+    arr.push([i, curr / total])
   }
-  return arr.sort((a, b) => b.fail - a.fail).map(e => e.name);
+  return arr.sort((a, b) => b[1] - a[1]).map(ele => ele[0]);
 }
