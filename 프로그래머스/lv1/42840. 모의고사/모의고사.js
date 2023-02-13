14ab6eeb401ab8eb9ee0573f8b21f2ab0697b1a4
Woody <86551233+kshlove735@users.noreply.github.com>
2023-02-13 15:25:52 +0900

    [level 1] Title: 모의고사, Time: 1.64 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,27 +1,17 @@
 function solution(answers) {
   let answer = [];
-  let pattern1 = "12345"
-  let pattern2 = "21232425"
-  let pattern3 = "3311224455"
-  let [...arr1] = answers.length > pattern1.length ? pattern1.repeat(Math.ceil((answers.length - pattern1.length) / pattern1.length) + 1) : pattern1;
-  let [...arr2] = answers.length > pattern2.length ? pattern2.repeat(Math.ceil((answers.length - pattern2.length) / pattern2.length) + 1) : pattern2;
-  let [...arr3] = answers.length > pattern3.length ? pattern3.repeat(Math.ceil((answers.length - pattern3.length) / pattern3.length) + 1) : pattern3;
+  let pattern1 = [1, 2, 3, 4, 5];
+  let pattern2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  let pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-  let arr = [{ name: 1, socore: 0 }, { name: 2, socore: 0 }, { name: 3, socore: 0 }]
+  let arr1 = answers.filter((num, i) => num === pattern1[i % pattern1.length]).length;
+  let arr2 = answers.filter((num, i) => num === pattern2[i % pattern2.length]).length;
+  let arr3 = answers.filter((num, i) => num === pattern3[i % pattern3.length]).length;
 
-  answers.forEach((num, i) => {
-    if (num === Number(arr1[i])) arr[0].socore++;
-    if (num === Number(arr2[i])) arr[1].socore++;
-    if (num === Number(arr3[i])) arr[2].socore++;
-  })
-  arr.sort((a, b) => b.socore - a.socore);
-  answer.push(arr[0].name)
+  let max = Math.max(arr1, arr2, arr3);
 
-  if (arr[0].socore === arr[1].socore) {
-    answer.push(arr[1].name)
-    if (arr[1].socore === arr[2].socore) answer.push(arr[2].name)
-  }
-
-  answer.sort((a, b) => a - b)
+  if (max === arr1) answer.push(1)
+  if (max === arr2) answer.push(2)
+  if (max === arr3) answer.push(3)
   return answer;
 }
