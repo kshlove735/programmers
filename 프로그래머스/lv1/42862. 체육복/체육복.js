bceef612bd27afe40928e9f56cf89d3b369364e0
Woody <86551233+kshlove735@users.noreply.github.com>
2023-02-14 20:13:25 +0900

    [level 1] Title: 체육복, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -0,0 +1,32 @@
+function solution(n, lost, reserve) {
+  lost.sort()
+  reserve.sort()
+
+  let set = new Set()
+  let lostObj = {}
+  lost.forEach(e => lostObj[e] = e)
+
+  for (let ele of reserve) {
+
+    set.add(ele)
+    if (ele === lostObj[ele]) continue;
+
+    if (lostObj[ele - 1] && lostObj[ele + 1]) {
+      if (!set.has(ele - 1)) { set.add(ele - 1); continue; }
+      if (!set.has(ele + 1)) { set.add(ele + 1); continue; }
+    }
+
+    if (lostObj[ele + 1]) {
+      if (!set.has(ele + 1)) { set.add(ele + 1); continue; }
+    }
+
+    if (lostObj[ele - 1]) {
+      if (!set.has(ele - 1)) { set.add(ele - 1); continue; }
+    }
+  }
+
+  for (let i = 1; i <= n; i++) {
+    if (!lostObj[i]) set.add(i);
+  }
+  return set.size;
+}
