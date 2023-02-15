7f10b299bda937c6772eca6570f0d02495660405
Woody <86551233+kshlove735@users.noreply.github.com>
2023-02-15 23:13:14 +0900

    [level 1] Title: 직사각형 별찍기, Time: 44.00 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,13 +2,8 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
   const n = data.split(" ");
   const a = Number(n[0]), b = Number(n[1]);
-
-  let answer = ''
+  let answer = '*'.repeat(a)
   for (let i = 0; i < b; i++) {
-    for (let j = 0; j < a; j++) {
-      answer += '*'
-    }
-    answer += '\n';
+    console.log(answer);
   }
-  console.log(answer);
 });
