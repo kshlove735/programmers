c8618cf9e08b4685a9246e337b8aeb78da7c5c0f
Woody <86551233+kshlove735@users.noreply.github.com>
2023-02-21 14:49:46 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -2,12 +2,12 @@ function solution(brown, yellow) {
   let x = 0;
   let y = 0;
   for (let i = 1; i <= yellow; i++) {
-    y = yellow / i
-    if (brown - 4 === (y + i) * 2 && yellow % i === 0) {
+   
+    if (brown - 4 === (yellow / i + i) * 2) {
       x = i;
+      y = yellow / i
       break;
     }
-    y = 0
   }
   return [Math.max(x + 2, y + 2) , Math.min(x + 2, y + 2)]
 }
