c3e7d67f01e9c9afbc931a126a792d5a0f15fef3
Woody <86551233+kshlove735@users.noreply.github.com>
2023-02-22 00:56:03 +0900

    [level 2] Title: 예상 대진표, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,9 @@
+function solution(n, a, b) {
+    let count = 0;
+    while (a !== b) {
+        a = Math.floor(a / 2) + a % 2
+        b = Math.floor(b / 2) + b % 2
+        count++
+    }
+    return count;
+}
