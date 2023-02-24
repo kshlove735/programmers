a405b1e7193c4d50b18e56c73f9fc6ca86035154
Woody <86551233+kshlove735@users.noreply.github.com>
2023-02-24 21:10:02 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,14 +1,7 @@
 function solution(n) {
-
-  function helper(n) {
-    let div = Math.floor(n / 2);
-    if (div === 0 && n % 2 === 0) return 0;
-    if (div === 0 && n % 2 !== 0) return 1;
-    if (n % 2 === 0) {
-      return helper(div);
-    } else {
-      return helper(div) + 1;
-    }
-  }
-  return helper(n)
+  let div = Math.floor(n / 2);
+  if (div === 0 && n % 2 === 0) return 0;
+  if (div === 0 && n % 2 !== 0) return 1;
+  if (n % 2 === 0) return solution(div);
+  else return solution(div) + 1;
 }
