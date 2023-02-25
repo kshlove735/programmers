ca587a69d322dc7f9211193e0a50880ae8fbbb82
Woody <86551233+kshlove735@users.noreply.github.com>
2023-02-26 02:17:20 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,10 +1,4 @@
 function solution(citationsn) {
-
-  let arr = [];
-  for (let i = 0; i < citationsn.length; i++) {
-    let ele = citationsn[i];
-    if (citationsn.filter(e => e >= ele).length === ele) arr.push(ele);
-  }
-  arr.sort((a, b) => b - a);
-  return arr[0]
+    citationsn.sort((a, b) => b - a);
+    return citationsn.filter((e, i) => e >= i + 1).length
 }
